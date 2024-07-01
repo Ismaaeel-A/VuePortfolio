a170ab93b618da091d608e313f539e4b2b92c95b
Ismaaeel-A <ismaaeelahmed42@gmail.com>
2024-07-01 16:02:56 +0200

    projects

---
+++ src/components/ProjectsSection.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div v-for="(Project, id) in Projects" :key="id">
+        <h5>{{ Project.title }}</h5>
+        <p>{{ Project.description }}</p>
+        
+        <a :href="Project.github" target="_blank"
+          ><button type="button">Github</button>
+        </a>
+        <a :href="Project.vercel" target="_blank"
+          ><button type="button">Vercel</button>
+        </a>
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed, onMounted } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore();
+const Projects = computed(() => store.state.Projects);
+
+onMounted(() => {
+  store.dispatch("fetchProjects");
+  console.log("EduExp Dispatch");
+});
+</script>
