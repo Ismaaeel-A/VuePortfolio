416547e54ab758769c8249272f11a8f68d4f9994
Ismaaeel-A <ismaaeelahmed42@gmail.com>
2024-07-02 09:33:29 +0200

    testimonial

---
+++ src/components/TestimonialsSection.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <h3>Testimonials</h3>
+
+      <Card v-for="(Testimonial, id) in Testimonials" :key="id">
+        <template #cardHeader>
+            <img :src="Testimonial.image" :alt="Testimonial.personName" class="img-fluid" />
+        </template>
+
+        <template #cardBody>
+            <h5>{{ Testimonial.personName }}</h5>
+            <p>{{ Testimonial.comment }}</p>
+        </template>
+      </Card>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed, onMounted } from "vue";
+import { useStore } from "vuex";
+import Card from "./CardComp.vue";
+
+const store = useStore();
+const Testimonials = computed(() => store.state.Testimonials);
+
+onMounted(() => {
+  store.dispatch("fetchTestimonials");
+  console.log("Testimonials Dispatch");
+});
+</script>
+
+<style>
+
+</style>
