136b121d3ccfce0a8361cb99c0ca1084e1e5c3b1
Ismaaeel-A <ismaaeelahmed42@gmail.com>
2024-09-19 08:19:19 +0200

    fixed navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg sticky-top">
+  <nav class="navbar navbar-expand-lg fixed-top">
     <div class="container-fluid">
       <a class="navbar-brand" href="#"><h2 logo>Ismaa'eel</h2></a>
       <button
